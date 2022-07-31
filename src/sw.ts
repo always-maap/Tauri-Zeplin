fe54b2d12973640343cf444f29435a8d1dd4b305
tur1ng <mohammadali.ap.2000@gmail.com>
2022-08-01 03:12:02 +0430

    fix: remove tsc and disable module isolation

--- src/sw.ts
+++ src/sw.ts
@@ -1,12 +1,10 @@
-export {};
-
-console.log("sw: registered");
-
 import { precacheAndRoute } from "workbox-precaching";
 
 declare let self: ServiceWorkerGlobalScope;
 
 precacheAndRoute(self.__WB_MANIFEST);
+
+console.log("sw: registered");
 
 self.addEventListener("install", () => {
   console.log("sw: installed");
